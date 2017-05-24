894c9394933e9339b05e4ea13d66b7c98d04b956
Théo LE GOFF <legoff.theo2@gmail.com>
2017-05-24 19:19:15 +0200

    Experimenting with sound

--- Assets/Scripts/AudioSourceLoudnessTester.cs
+++ Assets/Scripts/AudioSourceLoudnessTester.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioSourceLoudnessTester : MonoBehaviour {
 
 	public AudioSource audioSource;
+	public AudioMixer master;
 	public float updateStep = 0.1f;
 	public int sampleDataLength = 1024;
 	public float loudnessThreshold = 0.01f;
+	public int timeAheadToLookAt;
+	public float freqMax;
+	public float freqMin;
+	public float freqStep;
 
 	private float currentUpdateTime = 0f;
 
@@ -28,18 +34,30 @@ public class AudioSourceLoudnessTester : MonoBehaviour {
 		currentUpdateTime += Time.deltaTime;
 		if (currentUpdateTime >= updateStep) {
 			currentUpdateTime = 0f;
-			audioSource.clip.GetData(clipSampleData, audioSource.timeSamples); //I read 1024 samples, which is about 80 ms on a 44khz stereo clip, beginning at the current sample position of the clip.
+			audioSource.clip.GetData(clipSampleData, audioSource.timeSamples + timeAheadToLookAt*12800); //I read 1024 samples, which is about 80 ms on a 44khz stereo clip, beginning at the current sample position of the clip.
 			clipLoudness = 0f;
 			foreach (var sample in clipSampleData) {
 				clipLoudness += Mathf.Abs(sample);
 			}
 			clipLoudness /= sampleDataLength; //clipLoudness is what you are looking for
-			//Debug.Log(clipLoudness);
+			Debug.Log(clipLoudness);
 		}
 		if (clipLoudness > loudnessThreshold) {
-			//Debug.Log ("ShutDown all other clips");
-		} else {
-			//Debug.Log ("Activate all other clips");
+			float freq;
+			master.GetFloat("lowpassfreq", out freq);
+			if (freq > freqMin) {
+				freq -= freq/freqStep;
+				master.SetFloat("lowpassfreq", freq);
+			}
+
+		} 
+		else {
+			float freq;
+			master.GetFloat("lowpassfreq", out freq);
+			if (freq < freqMax) {
+				freq += freq/freqStep;
+				master.SetFloat("lowpassfreq", freq);
+			}
 		}
 
 	}
